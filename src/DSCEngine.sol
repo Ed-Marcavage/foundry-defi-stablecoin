b9cd6e1abdbf51ed1f02b0733b877c0646cf3e24
Ed Marcavage <edmarcavage256@proton.me>
2023-08-13 22:11:23 -0400

    init DSCEngine

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -21,3 +21,63 @@
 // external & public view & pure functions
 
 // SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+/*
+ * @title DSCEngine
+ * @author Ed Marcavage thanks to Patrick Collins
+ *
+ * The system is designed to be as minimal as possible, and have the tokens maintain a 1 token == $1 peg at all times.
+ * This is a stablecoin with the properties:
+ * - Exogenously Collateralized
+ * - Dollar Pegged
+ * - Algorithmically Stable
+ *
+ * It is similar to DAI if DAI had no governance, no fees, and was backed by only WETH and WBTC.
+ *
+ * @notice This contract is the core of the Decentralized Stablecoin system. It handles all the logic
+ * for minting and redeeming DSC, as well as depositing and withdrawing collateral.
+ * @notice This contract is based on the MakerDAO DSS system
+ */
+contract DSCEngine {
+    //////////////
+    // Errors
+    //////////////
+
+    error DSCEngine__NeedsMoreThanZero();
+
+    //////////////
+    // Modifiers
+    //////////////
+
+    modifier moreThanZero(uint256 _amount) {
+        if (_amount == 0) {
+            revert DSCEngine__NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    //////////////
+    // Functions
+    //////////////
+
+    constructor() {}
+
+    // depositCollateralAndMintDsc
+    function depositCollateralAndMintDsc() external {}
+
+    /*
+     * @notice This function allows a user to deposit collateral and mint DSC
+     * @param tokenCollateralAddress The address of the collateral token
+     * @param amountCollateral The amount of collateral to deposit
+     **/
+    function depositCollateral(
+        address tokenCollateralAddress,
+        uint256 amountCollateral
+    ) external moreThanZero(amountCollateral) {}
+    // redeemCollateralForDsc
+    // redeemCollateral
+    // burnDsc
+    // liquidate
+}
--- test/TestDecentralizedStableCoin.sol
+++ test/TestDecentralizedStableCoin.sol
@@ -9,13 +9,19 @@ contract DecentralizedStableCoinTest is Test {
     DeployDecentralizedStableCoin public deployDecentralizedStableCoin;
     DecentralizedStableCoin public decentralizedStableCoin;
 
+    address public constant USER = address(1);
+    uint256 public constant SEND_VALUE = 0.1 ether;
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+
     function setUp() public {
         deployDecentralizedStableCoin = new DeployDecentralizedStableCoin();
         decentralizedStableCoin = deployDecentralizedStableCoin.run();
     }
 
     function testBurnMustBeMoreThanZero() public {
-        vm.expectRevert();
-        decentralizedStableCoin.burn(0);
+        hoax(USER, STARTING_USER_BALANCE);
+        // vm.prank(USER);
+        //vm.expectRevert();
+        decentralizedStableCoin.burn(SEND_VALUE);
     }
 }
